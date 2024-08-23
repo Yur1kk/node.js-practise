ca28be85bce4c9b5d58239eaaa66219be29ed08e
Yurii Soroka <urasor04@gmail.com>
2024-08-23 15:38:17 +0300

    Http practise file

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.write('<h1>hello there!</h1>');
+  response.end();
+});
+
+server.listen(3000);
